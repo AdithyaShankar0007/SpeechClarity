45323a4b9401d0c1ef21db6a1e9f8318d2440782
AdithyaShankar0007 <145670341+AdithyaShankar0007@users.noreply.github.com>
2024-04-12 12:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from backend import recognize_speech
+import numpy as np
+from backend import recognize_speech, convert_to_pcm_wav
 from googletrans import Translator
 
 def main():
@@ -16,20 +17,26 @@ def main():
 
     if uploaded_file is not None:
         # Save the uploaded file to a temporary location
-        with open("temp_audio.wav", "wb") as f:
+        with open("temp_audio", "wb") as f:
             f.write(uploaded_file.read())
 
+        # Convert uploaded file to PCM WAV format
+        pcm_wav_file = convert_to_pcm_wav("temp_audio")
+        
         # Display the uploaded audio file
-        st.audio("temp_audio.wav", format="audio/wav")
+        st.audio(pcm_wav_file, format="audio/wav")
 
         # Language selection dropdown with options for different languages
         language = st.selectbox("Select language", ["Hindi", "Malayalam", "English", "Tamil"])
 
         # Button to trigger transcription
         if st.button("Transcribe"):
-            # Call the backend function with selected language
-            recognized_text = recognize_speech("temp_audio.wav", language=language.lower())
-            st.session_state.recognized_text = recognized_text
+            try:
+                # Call the backend function with selected language
+                recognized_text = recognize_speech(pcm_wav_file, language=language.lower())
+                st.session_state.recognized_text = recognized_text
+            except Exception as e:
+                st.error(f"Error during transcription: {e}")  # Display error message
 
     # Display transcribed text
     if st.session_state.recognized_text is not None:
@@ -40,13 +47,16 @@ def main():
         if st.session_state.recognized_text:
             # Language selection dropdown for translation
             translation_language = st.selectbox("Select translation language", ["Hindi", "Malayalam", "English", "Tamil"])
-            
+
             # Button to trigger translation
             if st.button("Translate"):
-                # Translate the text
-                translator = Translator()
-                translated_text = translator.translate(st.session_state.recognized_text, dest=translation_language.lower()).text
-                st.session_state.translated_text = translated_text
+                try:
+                    # Translate the text
+                    translator = Translator()
+                    translated_text = translator.translate(st.session_state.recognized_text, dest=translation_language.lower()).text
+                    st.session_state.translated_text = translated_text
+                except Exception as e:
+                    st.error(f"Error during translation: {e}")
 
     # Display translated text
     if st.session_state.translated_text is not None:
